5d113baba6cf33c40ce553ab18bf146b0e29fef4
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:28:38 +0330

    Add a base class to use for all API controllers

---
+++ WebApi/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers {
+
+    /// <summary>
+    /// Base class for all API controller of CoreGres project
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiController : ControllerBase {
+    }
+}
