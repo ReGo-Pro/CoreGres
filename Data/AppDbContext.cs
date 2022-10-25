f7225fda1d4c578de1da9c7ffda537da51368bc0
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 12:20:17 +0330

    Make AppDbContext public

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Domain.Core;
 using Microsoft.EntityFrameworkCore;
 
 namespace data {
-    internal class AppDbContext : DbContext {
+    public class AppDbContext : DbContext {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public AppDbContext() { }
