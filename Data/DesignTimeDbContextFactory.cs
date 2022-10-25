2208ad6117dd1c75078d82cb364d509cf5e9d3b0
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:59:02 +0330

    Add a class required for creating design-time context for migrations

---
+++ Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace data {
+    internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext> {
+        public AppDbContext CreateDbContext(string[] args) {
+            var designDbConnex = "User ID=root;Password=123456;Host=localhost;Port=5432;Database=CoreGresDb;";
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseNpgsql(designDbConnex);
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
