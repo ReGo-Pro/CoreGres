c8400ca2bf7b7f8d62cf7d3163b0679444ebfd37
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:06:09 +0330

    Add a default (home) controller

---
+++ WebApi/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return Ok("API is working properly.");
+        }
+    }
+}
