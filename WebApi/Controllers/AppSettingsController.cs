56719624e4535e9a8e8901c02640f07a7e3d56ea
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:40:09 +0330

    Change AppSettingController to return ViewModel instead of model itself

--- WebApi/Controllers/AppSettingsController.cs
+++ WebApi/Controllers/AppSettingsController.cs
@@ -1,16 +1,18 @@
 ﻿using Domain.Core;
 using Microsoft.AspNetCore.Mvc;
+using webapi.Mappers;
 
 namespace webapi.Controllers {
     public class AppSettingsController : ApiController {
 
         [HttpGet("")]
         public IActionResult GetAllSettings() {
-            return Ok(new List<AppSetting>()
-            {
+            var result = new List<AppSetting>() {
                 new AppSetting() { ID = 1, Key = "Owner", Value = "ReGo"},
                 new AppSetting() { ID = 2, Key = "IsDefault", Value = "Yes"}
-            });
+            };
+
+            return Ok(result.Select(s => s.ToDto()));
         }
     }
 }
