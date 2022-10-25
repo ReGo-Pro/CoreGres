07adf91bfc41e3344fdb2919f652fbd7efd428b4
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:39:39 +0330

    Add Mapper to map AppSetting to/from ViewModel

---
+++ WebApi/Mappers/AppSettingMapper.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Core;
+using webapi.ViewModels.AppSettings;
+
+// I might use AutoMapper instead
+namespace webapi.Mappers {
+    public static class AppSettingMapper {
+        public static AppSettingViewModel ToDto(this AppSetting model) {
+            return new AppSettingViewModel
+            {
+                ID = model.ID,
+                Key = model.Key,
+                Value = model.Value
+            };
+        }
+
+        public static AppSetting ToModel(this AppSettingCreationViewModel dto) {
+            return new AppSetting()
+            {
+                Key = dto.Key,
+                Value = dto.Value
+            };
+        }
+    }
+}
