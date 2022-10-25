11a279f3b9b3ebaf6bcd8f337453f8fc134e63fd
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:18:57 +0330

    Add AppSetting class to Domain project

---
+++ Domain/Core/AppSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Core {
+    public class AppSetting {
+        public int ID { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
