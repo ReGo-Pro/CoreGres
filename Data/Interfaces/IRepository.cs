33c88f45850131c0d4cd0e337b343fb8bc76db62
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 12:10:20 +0330

    Add generic repository interface to Data project

---
+++ Data/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data.Interfaces {
+    public interface IRepository<TEntity, TIdentifier> where TEntity : class {
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        TEntity FindById(TIdentifier ID);
+    }
+}
