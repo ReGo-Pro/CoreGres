745c184963c8156f951bfac97e8a327cfa1a266c
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:32:58 +0330

    Add AppSettingsController with a demo get method

---
+++ WebApi/Controllers/AppSettingsController.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers {
+    public class AppSettingsController : ApiController {
+
+        [HttpGet("")]
+        public IActionResult GetAllSettings() {
+            return Ok(new List<AppSetting>()
+            {
+                new AppSetting() { ID = 1, Key = "Owner", Value = "ReGo"},
+                new AppSetting() { ID = 2, Key = "IsDefault", Value = "Yes"}
+            });
+        }
+    }
+}
