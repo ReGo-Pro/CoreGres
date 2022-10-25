ca20f8a06134e6622f264ffc74ba65ac41c753bd
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:26:06 +0330

    Add AppDbContext with DI-friendly constructor

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace data {
+    internal class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
