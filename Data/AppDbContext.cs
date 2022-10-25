6d38ddeed9565fd8e0093449a18256d2bc5af344
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:59:37 +0330

    Add a parameterless constructor to AppDbContext for design time migrations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,6 +5,8 @@ namespace data {
     internal class AppDbContext : DbContext {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public AppDbContext() { }
+
         public DbSet<AppSetting> AppSettings { get; set; }
     }
 }
