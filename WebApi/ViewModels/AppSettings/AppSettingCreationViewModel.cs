0e5542a1c85dfef200fd611e9bc3952700ca373c
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 13:48:20 +0330

    Add validation attributes to AppSettingCreationViewModel

--- WebApi/ViewModels/AppSettings/AppSettingCreationViewModel.cs
+++ WebApi/ViewModels/AppSettings/AppSettingCreationViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace webapi.ViewModels.AppSettings {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi.ViewModels.AppSettings {
+    // TODO: I might use FluentValidation for these
     public class AppSettingCreationViewModel {
+        [Required]
+        [MaxLength(256)]
         public string Key { get; set; }
+
+        [Required]
+        [MaxLength(256)]
         public string Value { get; set; }
     }
 }
