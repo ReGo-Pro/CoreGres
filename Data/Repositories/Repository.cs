629fc473750fde559a15dfe587dc698b2f80e240
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 12:13:05 +0330

    Add a general implementation of IRepository to Data project

---
+++ Data/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace data.Repositories {
+    public class Repository<TEntity, TIdentifier> : IRepository<TEntity, TIdentifier> where TEntity : class {
+        private DbContext _context;
+
+        public Repository(DbContext context) {
+            _context = context;
+        }
+
+        public void Add(TEntity entity) {
+            _context.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities) {
+            _context.AddRange(entities);
+        }
+
+        public TEntity FindById(TIdentifier ID) {
+            var entity = _context.Find(typeof(TEntity), ID);
+            if (entity == null) {
+                return null;
+            }
+
+            return (TEntity)entity;
+        }
+
+        public void Remove(TEntity entity) {
+            _context.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities) {
+            _context.RemoveRange(entities);
+        }
+
+        protected virtual DbContext Context => _context;
+    }
+}
