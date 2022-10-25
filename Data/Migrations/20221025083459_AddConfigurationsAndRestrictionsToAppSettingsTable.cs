1f6be8610b26dcce10a1ff22a435dffa2f3e20e1
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 12:05:34 +0330

    Add configurations and restrictions To AppSettings table using migrations

---
+++ Data/Migrations/20221025083459_AddConfigurationsAndRestrictionsToAppSettingsTable.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace data.Migrations
+{
+    public partial class AddConfigurationsAndRestrictionsToAppSettingsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "AppSettings",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "AppSettings",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppSettings_Key",
+                table: "AppSettings",
+                column: "Key",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppSettings_Key",
+                table: "AppSettings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "AppSettings",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "AppSettings",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+        }
+    }
+}
