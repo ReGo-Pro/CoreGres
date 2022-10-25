7cb8c74906e6c98c1e7aa69834eb1d661d1d0a92
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 11:47:26 +0330

    Add endpoint to get a single AppSetting by key

--- WebApi/Controllers/AppSettingsController.cs
+++ WebApi/Controllers/AppSettingsController.cs
@@ -4,15 +4,29 @@ using webapi.Mappers;
 
 namespace webapi.Controllers {
     public class AppSettingsController : ApiController {
+        List<AppSetting> settings = new List<AppSetting>() {
+            new AppSetting() { ID = 1, Key = "Owner", Value = "ReGo"},
+            new AppSetting() { ID = 2, Key = "IsDefault", Value = "Yes"}
+        };
 
         [HttpGet("")]
         public IActionResult GetAllSettings() {
-            var result = new List<AppSetting>() {
-                new AppSetting() { ID = 1, Key = "Owner", Value = "ReGo"},
-                new AppSetting() { ID = 2, Key = "IsDefault", Value = "Yes"}
-            };
+            return Ok(settings.Select(s => s.ToDto()));
+        }
 
-            return Ok(result.Select(s => s.ToDto()));
+        [HttpGet("{key}")]
+        public IActionResult GetSetting(string key) {
+            if (string.IsNullOrEmpty(key)) {
+                return BadRequest();
+            }
+
+            var result = settings.Where(s => s.Key.Equals(key, StringComparison.OrdinalIgnoreCase)).SingleOrDefault();
+            
+            if (result == null) {
+                return NotFound();
+            }
+
+            return Ok(result.ToDto());
         }
     }
 }
