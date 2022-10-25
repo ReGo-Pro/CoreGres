f4365228f11f03039b382efba26b258644aa4776
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-25 12:02:34 +0330

    Add configuration class for AppSettings

---
+++ Data/EntityConfigurations/AppSettingEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace data.EntityConfigurations {
+    public class AppSettingEntityConfiguration : IEntityTypeConfiguration<AppSetting> {
+        public void Configure(EntityTypeBuilder<AppSetting> builder) {
+            builder.Property(e => e.Key)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            builder.HasIndex(x => x.Key)
+                .IsUnique();
+
+            builder.Property(e => e.Value)
+                .IsRequired()
+                .HasMaxLength(256);
+        }
+    }
+}
