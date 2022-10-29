5b312a7b49cb594e589cb0f292ef78d9076872a4
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-29 23:15:20 +0330

    Add appsettings to Migrator project to have a default connection string in case environment variables are not provided

--- DbMigrator/Program.cs
+++ DbMigrator/Program.cs
@@ -1,18 +1,31 @@
 ﻿using data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
-var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "root";
-var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "123456";
-var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "CoreGres";
-var dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost";
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 
-var connectionString = $"Host={dbHost};Port=5432;User ID={dbUser};Password={dbPassword};Database={dbName};";
+string dbConnex = string.Empty;
+
+if ((dbUser ?? dbPassword ?? dbName ?? dbHost) == null) {
+    var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+    dbConnex = config.GetConnectionString("AppDbConnex");
+}
+else {
+    dbConnex = $"Host={dbHost};Port=5432;User ID={dbUser};Password={dbPassword};Database={dbName};";
+}
 
 try {
-    var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>().UseNpgsql(connectionString);
+    var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>().UseNpgsql(dbConnex);
     using (var context = new AppDbContext(optionsBuilder.Options)) {
         if ((await context.Database.GetPendingMigrationsAsync()).Any()) {
             await context.Database.MigrateAsync();
+            Console.WriteLine("All migrations are applied successfully.");
+        }
+        else {
+            Console.WriteLine("No pending migrations found. The database is already up to date.");
         }
     }
 }
