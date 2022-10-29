a5c91598580fa76ac045243d9489dba9a006b524
Reza Ghochkhani <reza.ghochkhani@specific-group.com>
2022-10-30 01:53:02 +0330

    Change CoreGresDb to CoreGres

--- Data/DesignTimeDbContextFactory.cs
+++ Data/DesignTimeDbContextFactory.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Design;
 namespace data {
     internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext> {
         public AppDbContext CreateDbContext(string[] args) {
-            var designDbConnex = "User ID=root;Password=123456;Host=localhost;Port=5432;Database=CoreGresDb;";
+            var designDbConnex = "User ID=root;Password=123456;Host=localhost;Port=5432;Database=CoreGres;";
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
             optionsBuilder.UseNpgsql(designDbConnex);
             return new AppDbContext(optionsBuilder.Options);
